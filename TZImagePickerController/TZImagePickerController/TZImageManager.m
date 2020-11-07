1379a8d3e65f028c8ee22afa467624ee9794d39a
klone <zhanglongqing@theduapp.com>
2020-11-07 21:55:22 +0800

    fix: 少个 )

--- TZImagePickerController/TZImagePickerController/TZImageManager.m
+++ TZImagePickerController/TZImagePickerController/TZImageManager.m
@@ -145,7 +145,7 @@ static dispatch_once_t onceToken;
             // 有可能是PHCollectionList类的的对象，过滤掉
             if (![collection isKindOfClass:[PHAssetCollection class]]) continue;
             // 过滤空相册
-            if (collection.estimatedAssetCount <= 0 && ![self isCameraRollAlbum:collection]) && (collection.estimatedAssetCount != NSNotFound) continue;
+            if (collection.estimatedAssetCount <= 0 && ![self isCameraRollAlbum:collection] && (collection.estimatedAssetCount != NSNotFound)) continue;
             PHFetchResult *fetchResult = [PHAsset fetchAssetsInAssetCollection:collection options:option];
             if (fetchResult.count < 1 && ![self isCameraRollAlbum:collection]) continue;
             
